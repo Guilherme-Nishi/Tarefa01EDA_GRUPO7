a2a6fe7e48d294d2044ee4800297ea589b796e9b
200030264-Guilherme.Nishimura <78215376+Guilherme-Nishi@users.noreply.github.com>
2022-02-23 22:36:16 -0300

    main.c
    Commit com inicio da main

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#include <math.h>
+
+
+int main(){
+	
+int menu;
+
+
+do{
+
+printf("Escolha uma das opcoes do menu\n ");
+printf("1.Leitura dos arquivos\n");
+printf("2.opcao 2\n");
+printf("opcao 3\n");
+printf("opcao 4\n");
+printf("opcao 5\n");	
+scanf("%d",menu);
+}while(menu!=5);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
