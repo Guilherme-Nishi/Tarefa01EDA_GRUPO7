4f772c6fd4c6c42d7a46ba066d4e5d58ea7081dd
matheusrazor <53947083+matheusrazor@users.noreply.github.com>
2022-02-25 22:39:12 -0300

    WordCounterAndVectorFiller funcao criada. Conta o numero de palavras no arquivo e as preenche num vetor, para facilitar a manipulacao futuramente.

--- tfidf.c
+++ tfidf.c
@@ -1,5 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+
 typedef struct 
 {
     char word[100];
-    char score[10];
+    int score;
 } WordTFIDF;
+
+int wordCounterAndVectorFiller(FILE *arc, char *wordVector)
+{
+    int i = 0, wordCounter = 0;
+    int aux = 0;
+    char newWord[100], c;
+    while ((c = fgetc(arc)) != EOF)
+    {
+        if (c >= 65 && c <= 90) 
+        {
+            c += 32;
+        } 
+        if (c >= 97 && c <= 122) 
+        {
+            newWord[i] = c;
+            ++i;
+        }
+        else
+        {
+            aux = 1;
+        }
+        if (aux)
+        {
+            if (i > 3)
+            {
+                for (int j = 0; j < i; j++) {
+                    *wordVector[wordCounter][j] = newWord[j];
+                }
+            }
+            ++wordCounter;
+            i = 0;
+        }
+    }
+    return wordCounter;
+}
